147efe6ea58798bda0d91b93030beb49aaebcc94
wanessabarroslino <wanessabarroslino@gmail.com>
2023-12-18 12:01:20 -0300

    Att

--- src/components/SocialNetworks.tsx
+++ src/components/SocialNetworks.tsx
@@ -14,7 +14,7 @@ const SocialNetworks = () => {
   return (
     <section id="social-networks">
         {socialNetworks.map((network) => (
-            <a href={network.link} className='social-btn' id={network.name} key={network.name}>
+            <a href={network.link} className='social-btn' id={network.name} key={network.name} target="_blank" rel="noopener noreferrer">
                 {network.icon}
             </a>
         ))}
