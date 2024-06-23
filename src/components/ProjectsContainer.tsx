bcd5cc73503a8fd8bfede7a3a412c206b6021174
Wanessa Barros Lino <108156873+wanessabarroslino@users.noreply.github.com>
2024-06-23 12:29:03 -0300

    alterações simples

--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './../styles/components/projectscontainer.sass';
+
 const ProjectsContainer = () => {
   return (
     <section className="projects-container">
@@ -7,12 +9,15 @@ const ProjectsContainer = () => {
       <p>
         Ainda em construção... Logo mais você verá muitos projetos meus aqui!
       </p>
-      <a href='https://midi-murex.vercel.app/' target='_blank' rel="noopener noreferrer" className='btn'>
-        midi
-      </a>
-      <a href='https://secretword-inky.vercel.app/' target='_blank' rel="noopener noreferrer" className='btn'>
-        secret word game
-      </a>
+      <br></br>
+      <div className='projects'>
+        <a href='https://midi-murex.vercel.app/' target='_blank' rel="noopener noreferrer" className='btn'>
+          midi
+        </a>
+        <a href='https://secretword-inky.vercel.app/' target='_blank' rel="noopener noreferrer" className='btn'>
+          secret word game
+        </a>
+      </div>  
     </section>
   )
 }
